d7573f907f25b6803769a9020a3f2c7b71510e60
lcalitz <louis.calitz@gmail.com>
2011-04-14 17:03:40 -0400

    Added Porcfest vendor, etc. changes

---
+++ sites/all/themes/porcfest/templates/node/node-vendor.tpl.php
@@ -0,0 +1,84 @@
+<?php 
+// $Id: node.tpl.php,v 1.1.2.8 2009/05/19 00:05:00 jmburnz Exp $
+
+/**
+ * @file node.tpl.php
+ * Theme implementation to display a node.
+ *
+ * Available variables:
+ * - $title: the (sanitized) title of the node.
+ * - $content: Node body or teaser depending on $teaser flag.
+ * - $picture: The authors picture of the node output from
+ *   theme_user_picture().
+ * - $date: Formatted creation date (use $created to reformat with
+ *   format_date()).
+ * - $links: Themed links like "Read more", "Add new comment", etc. output
+ *   from theme_links().
+ * - $name: Themed username of node author output from theme_user().
+ * - $node_url: Direct url of the current node.
+ * - $terms: the themed list of taxonomy term links output from theme_links().
+ * - $submitted: themed submission information output from
+ *   theme_node_submitted().
+ *
+ * Other variables:
+ * - $node: Full node object. Contains data that may not be safe.
+ * - $type: Node type, i.e. story, page, blog, etc.
+ * - $comment_count: Number of comments attached to the node.
+ * - $uid: User ID of the node author.
+ * - $created: Time the node was published formatted in Unix timestamp.
+ * - $zebra: Outputs either "even" or "odd". Useful for zebra striping in
+ *   teaser listings.
+ * - $id: Position of the node. Increments each time it's output.
+ *
+ * Helper variables:
+ * - $node_id: Outputs a unique id for each node.
+ * - $classes: Outputs dynamic classes for advanced themeing.
+ *
+ * Node status variables:
+ * - $teaser: Flag for the teaser state.
+ * - $page: Flag for the full page state.
+ * - $promote: Flag for front page promotion state.
+ * - $sticky: Flags for sticky post setting.
+ * - $status: Flag for published status.
+ * - $comment: State of comment settings for the node.
+ * - $readmore: Flags true if the teaser content of the node cannot hold the
+ *   main body content.
+ * - $is_front: Flags true when presented in the front page.
+ * - $logged_in: Flags true when the current user is a logged-in member.
+ * - $is_admin: Flags true when the current user is an administrator.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_node()
+ * @see genesis_preprocess_node()
+ */
+?>
+<?php //dprint_rx(get_defined_vars()); ?>
+<div id="<?php print $node_id; ?>" class="<?php print $classes; ?>">
+  <div class="node-inner">
+    
+    <?php if (!$page): ?>
+      <h2 class="node-title">
+        <a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a>
+        <?php print $unpublished; ?>
+      </h2>
+    <?php endif; ?>
+
+    <?php if ($submitted): ?>
+      <div class="node-submitted"><?php print $submitted; ?></div>
+    <?php endif; ?>
+
+    <?php print $picture; ?>
+
+    <div class="node-content">
+      <a href="/vendors">&laquo; Back to Vendors</a>
+      <h1><?php print $node->field_vendor_business[0]['value']; ?></h1>
+      <?php print "<span id=\"vendor-info\">at Lot #". $node->field_vendor_lot_number[0]['value']. " by " .$title. " - <a href=".$node->field_vendor_website[0]['url'].">Go to ".$node->field_vendor_business[0]['value']."'s website</a></span>"; ?>
+      <br/><br/>
+      <?php print $node->body; ?>
+      
+      <a href="/vendors">&laquo; Back to Vendors</a>
+    </div>
+
+
+  </div>
+</div> <!-- /node -->
--- sites/all/themes/porcfest/templates/page/page.tpl.php
+++ sites/all/themes/porcfest/templates/page/page.tpl.php
@@ -98,7 +98,15 @@
  * @see layout.css
  */
 $background_data = porcfest_get_random_photo();
-?>        
+?>
+<?php if($logged_in){ ?>
+  <script type="text/javascript">
+    function gotonav() {
+      window.location = "#nav";
+    }
+    window.onload = gotonav;
+  </script>
+<?php } ?>
 <body id="genesis-1c" <?php print $section_class; ?>>
   <div id="background-photo" <?php print $background_data[1]; ?>>
     <div id="wrapper">
