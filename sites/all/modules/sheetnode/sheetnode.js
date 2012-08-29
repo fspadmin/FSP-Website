e8c5b70fa08b0cbf729add9c1f67f962e6aa4174
Louis Calitz <lcalitz@freestateproject.org>
2012-08-29 08:50:40 -0400

    Core & modules upgrade

--- sites/all/modules/sheetnode/sheetnode.js
+++ sites/all/modules/sheetnode/sheetnode.js
@@ -104,7 +104,32 @@ Drupal.sheetnode.prototype.functionsSetup = function() {
 }
 
 Drupal.sheetnode.prototype.loadsheetSetup = function() {
+  var self = this;
   SocialCalc.RecalcInfo.LoadSheet = function(sheetname) {
+    var found = false;
+
+    // Avoid returning this sheet.
+    if (self.settings.aliases.indexOf(sheetname) != -1) {
+      // TODO: Should really return true and inform SocialCalc that this is the same sheet.
+      // However SocialCalc does not support aliases currently.
+      return false;
+    }
+
+    // First examine on-screen spreadsheets.
+    $.each(Drupal.sheetnode.sheetviews, function(index, sheetnode) {
+      if (sheetnode.settings.aliases.indexOf(sheetname) != -1) {
+        var data = sheetnode.spreadsheet.CreateSpreadsheetSave();
+        var parts = sheetnode.spreadsheet.DecodeSpreadsheetSave(data);
+        if (parts.sheet) {
+          data = data.substring(parts.sheet.start, parts.sheet.end);
+          SocialCalc.RecalcLoadedSheet(sheetname, data, false, true);
+          found = true;
+        }
+      }
+    });
+    if (found) return true;
+
+    // Next call backend to find the reference.
     data = $.ajax({
       type: 'POST',
       url: Drupal.settings.basePath+'sheetnode/load',
@@ -114,8 +139,10 @@ Drupal.sheetnode.prototype.loadsheetSetup = function() {
     }).responseText;
     if (data !== null) {
       SocialCalc.RecalcLoadedSheet(sheetname, data, true);
+      found = true;
     }
-    return data;
+
+    return found;
   }
 }
 
@@ -347,4 +374,38 @@ parseUri.options = {
     loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
   }
 };
+
+// https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/indexOf#Compatibility
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+    "use strict";
+    if (this == null) {
+      throw new TypeError();
+    }
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (len === 0) {
+      return -1;
+    }
+    var n = 0;
+    if (arguments.length > 0) {
+      n = Number(arguments[1]);
+      if (n != n) { // shortcut for verifying if it's NaN
+          n = 0;
+      } else if (n != 0 && n != Infinity && n != -Infinity) {
+          n = (n > 0 || -1) * Math.floor(Math.abs(n));
+      }
+    }
+    if (n >= len) {
+      return -1;
+    }
+    var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+    for (; k < len; k++) {
+      if (k in t && t[k] === searchElement) {
+        return k;
+      }
+    }
+    return -1;
+  }
+}
 
